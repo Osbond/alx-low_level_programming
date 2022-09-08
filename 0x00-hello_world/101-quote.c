bd03a9e47817a55783314eac4c006fa2b6bd84fc
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-09-08 15:07:36 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print string without put and printf
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
