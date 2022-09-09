84be0c7c623e7b551adc36c3c32268658354ff52
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-09-09 15:14:27 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i == '9')
+			break;
+		putchar(',');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
