745c04f2c784842db5480004e5168248a3af5a7e
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-09-13 14:09:48 -0700

    first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print the sign in front of a number
+ *
+ * Return: 1(success), 0(failure)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
