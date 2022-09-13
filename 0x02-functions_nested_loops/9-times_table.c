726751483ee6ec0f88716c9901b8dc96ae68df44
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-09-13 15:48:56 -0700

    first commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - print the times table till 9
+ *
+ * Return: void;
+ */
+void times_table(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			int multiply;
+
+			multiply = i * j;
+			if (j == 0)
+				_putchar('0' + multiply);
+			else if (multiply < 10)
+			{
+				_putchar(' ');
+			i	_putchar('0' + multiply);
+			}
+			else
+			{
+				_putchar('0' + multiply / 10);
+				_putchar('0' + multiply % 10);
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
