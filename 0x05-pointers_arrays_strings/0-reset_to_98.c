ccd440600cf27b152360970900d065f3729ccb03
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-09-19 10:24:18 -0700

    firs commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - Function that takes a pointer and updates it s value
+ * @n: a pointer
+ *
+ * Return: void;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
