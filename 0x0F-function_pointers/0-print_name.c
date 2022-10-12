34e3a695a2f23c6a51c696a4ab03db81346baa2b
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-10-12 15:29:37 -0700

    2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,9 +9,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
 	void (*func_ptr)(char *) = f;
 
+	if (name == NULL || f == NULL)
+		return;
 	func_ptr(name);
 }
