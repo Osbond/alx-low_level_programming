c6afbf491440f2f3d3c90c1c632287510f992b89
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-10-13 13:06:47 -0700

    adjustment

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,9 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i = 0;
 
-	if (separator == NULL)
-		return;
-		
 	va_start(list, n);
 	while (i < n)
 	{
