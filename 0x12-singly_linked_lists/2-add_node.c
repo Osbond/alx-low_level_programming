b9c297157052a22c22265dce3efa7ecf62fea57b
Osborne Damilola <Osbornedamilola007@gmail.com>
2022-10-27 11:04:25 -0700

    initialised the count

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = malloc(sizeof(list_t));
-	int count;
+	int count = 0;
 
 	while (str[count] != '\0')
-	{
 		count++;
-	}
 	ptr->str = strdup(str);
-	ptr->len = count - 1;
+	ptr->len = count;
 	ptr->next = *head;
 	*head = ptr;
 	return (ptr);
